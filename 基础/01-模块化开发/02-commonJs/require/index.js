3ddae281d5cad24a3812eb73bc62123556af6815
TENCENT\v_llingai <1120877019@qq.com>
2021-08-17 16:49:19 +0800

    require执行流程

--- 基础/01-模块化开发/02-commonJs/require/index.js
+++
@@ -1,2 +0,0 @@
-let modulePahts = require.resolve.paths('asd'); //返回被查询的路径
-console.log(modulePahts);
---
+++ 基础/01-模块化开发/02-commonJs/require/main.js
@@ -0,0 +1,11 @@
+console.log("main")
+
+require('./aaa');
+require('./bbb');
+console.log(module);
+// main
+// aaa
+// ccc
+// ddd
+// eee
+// bbb
