6789af8e4169ad469f7b34c2e51e6addcf88d580
TENCENT\v_llingai <1120877019@qq.com>
2021-08-18 15:17:14 +0800

    es

---
+++ 基础/01-模块化开发/03-es_modules/特点/foo.js
@@ -0,0 +1,10 @@
+let age = 18;
+const obj = { id: 957 }
+
+setTimeout(() => {
+  age = 999
+}, 1000)
+export {
+  age,
+  obj
+}
---
+++ 基础/01-模块化开发/03-es_modules/特点/index.js
@@ -0,0 +1,5 @@
+import { age } from './foo.js';
+
+setTimeout(() => {
+  console.log(age);
+}, 2000)
