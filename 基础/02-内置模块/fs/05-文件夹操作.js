b7a1f2fe8ac0e4f169e3477f257987e637379a54
TENCENT\v_llingai <1120877019@qq.com>
2021-08-19 15:34:09 +0800

    文件夹操作1

--- 基础/02-内置模块/fs/05-文件夹操作.js
+++ 基础/02-内置模块/fs/05-文件夹操作.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
+const path = require('path');
 
 const dirname = './loveZero';
-// 方式一
+// 文件夹是否存在方式一
 // fs.existsSync('./loveZero')
 
-// 方式二
+// 文件夹是否存在方式二
 // fs.access(dirname, fs.constants.F_OK, (err) => {
 //   console.log(`${dirname} ${err ? '不存在' : '存在'}`);
 // });
@@ -28,3 +29,44 @@ const dirname = './loveZero';
 //     console.log(`${dirname} 存在，且可写`);
 //   }
 // });
+
+
+// 使用 fs.mkdir() 或 fs.mkdirSync() 可以创建新的文件夹。
+// if (!fs.existsSync(dirname)) {
+//   fs.mkdir(dirname, err => {
+//     console.log(err);
+//   });
+// }
+
+// 读取目录的内容
+// const folderPath = '../fs'
+// fs.readdir(folderPath, (err, files) => {
+//   console.log(files);
+// });
+// console.log(fs.readdirSync(folderPath));
+/*
+[
+  '01-获取文件状态.js',
+  '02-文件描述符.js',
+  '03-文件读取.js',
+  '04-文件写入.js',
+  '05-文件夹操作.js',
+  'abc.txt',
+  'loveZero'
+]*/
+
+
+function getFiles(dirname) {
+  fs.readdir(dirname, { withFileTypes: true }, (err, files) => {
+    for (let file of files) {
+      if (file.isDirectory()) {
+        const filepath = path.resolve(dirname, file.name);
+        getFiles(filepath);
+      } else {
+        console.log(file.name);
+      }
+    }
+  });
+}
+
+getFiles(dirname);
