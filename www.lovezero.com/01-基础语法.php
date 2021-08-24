86ce5d51d7fac462b85c2729fade18a7389236d4
loveZero <1120877019@qq.com>
2021-08-24 23:41:40 +0800

    基础语法

---
+++ www.lovezero.com/01-基础语法.php
@@ -0,0 +1,25 @@
+<?php
+    echo 111,222;
+    print 111;
+        // 这是单行注释
+    /*
+        这是多行注释
+        注释后，在浏览器和网页源码中，是看不到的。
+    */
+    echo '<hr>';
+    $a = '变量';
+    echo $a;
+    echo '<hr>';
+    
+    # 下划线命名法
+    $new_title = '下划线命名法';
+    echo $new_title;
+    echo '<hr>';
+    # 小驼峰命名法
+    $newTitle  = '小驼峰命名法';
+    echo $newTitle;
+    echo '<hr>';
+    # 大驼峰命名法
+    $NewTitle  = '大驼峰命名法';
+    echo $NewTitle;
+?>
