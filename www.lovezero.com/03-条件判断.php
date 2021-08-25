c1f0a07d8caad483d64bc4da3bf4e1c58c2bc0d7
loveZero <1120877019@qq.com>
2021-08-25 23:22:11 +0800

    条件判断

---
+++ www.lovezero.com/03-条件判断.php
@@ -0,0 +1,56 @@
+<?php
+// 条件判断
+$flag = true;
+var_dump($flag ? '真的' : '假的');
+echo '<hr />';
+
+// if
+if ($flag) {
+  echo $flag;
+}
+echo '<hr />';
+
+// if...else
+if ($flag) {
+  echo  'if:' . $flag;
+} else {
+  echo  'else:' . $flag;
+};
+echo '<hr />';
+
+$num = 2;
+// 
+if ($num === 1) {
+  echo  'if:' . $flag;
+} else if ($num === 2) {
+  echo  'else...if:' . $num;
+} else {
+  echo  'else:' . $num;
+}
+echo '<hr />';
+
+
+$str = '你好';
+switch ($str) {
+  case 'hello':
+    echo 'hello';
+  case '你好':
+    echo '你好';
+  default:
+    echo '哈哈';
+}
+echo '<hr />';
+
+$str = '你好';
+switch ($str) {
+  case 'hello':
+    echo 'hello';
+    break;
+  case '你好':
+    echo '你好';
+    break;
+
+  default:
+    echo '哈哈';
+}
+echo '<hr />';
