091dfaab25ebe4a57356074b487f34ee83ef395d
loveZero <1120877019@qq.com>
2021-09-03 23:00:35 +0800

    多态

---
+++ code/01-面向对象/04-多态.ts
@@ -0,0 +1,23 @@
+export { }
+abstract class Animal {
+  abstract speak(): void
+}
+
+class Dog extends Animal {
+  speak() {
+    console.log('汪汪汪');
+  }
+}
+
+class Cat extends Animal {
+  speak() {
+    console.log('喵喵喵');
+  }
+}
+
+function talk(animal: Animal) {
+  animal.speak()
+}
+
+talk(new Dog())
+talk(new Cat())
