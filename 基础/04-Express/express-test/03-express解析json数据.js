a80fce51f5611d4f9576e89433fbbb541acadf0d
TENCENT\v_llingai <1120877019@qq.com>
2021-11-25 17:02:53 +0800

    解析json数据

---
+++ 基础/04-Express/express-test/03-express解析json数据.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+// 使用express提供的body解析
+app.use(express.json());
+// extended
+// true: 使用qs解析urlencoded
+// false: 使用querystring解析urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/login', (req, res, next) => {
+  console.log(req.body);
+  res.end("请求成功");
+});
+
+app.listen(8000, () => {
+  console.log("express初体验服务器启动成功~");
+});
