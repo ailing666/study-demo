02a297aa255479837708630fbdf11b9e99e627dc
loveZero <1120877019@qq.com>
2021-12-09 23:29:15 +0800

    组件

--- 01-HelloReact/hello-react/src/app.js
+++ 01-HelloReact/hello-react/src/app.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react'
 export default class App extends Component {
   render () {
-    return <h2>Hello</h2>
+    return [<h2>Hello</h2>, <h2>Hello</h2>]
   }
 }
+
+// export default function App () {
+//   return (
+//     <div>
+//       <h2>Hello</h2>
+//     </div>
+//   )
+// }
