f02b1b5ab39b04d3e4cc036ab73f6880a15265b6
loveZero <1120877019@qq.com>
2021-12-12 21:15:21 +0800

    PropTypes

--- 01-HelloReact/hello-react/src/Son.js
+++ 01-HelloReact/hello-react/src/Son.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
-
+import PropTypes from 'prop-types'
 // 类组件
 class ChildCpn extends Component {
-  componentDidMount () {
-    console.log(this.props, 'componentDidMount')
-  }
+  // es6中的class fields写法
+  // static propTypes = {
+  //   // 字符串类型，必填
+  //   name: PropTypes.string.isRequired,
+  //   age: PropTypes.number,
+  //   height: PropTypes.string
+  // }
+
+  // static defaultProps = {
+  //   name: 'loveZero',
+  //   age: 18,
+  //   height: '1.58'
+  // }
 
   render () {
     // 通过this.props拿到父组件传来的数据
@@ -13,6 +23,20 @@ class ChildCpn extends Component {
   }
 }
 
+// 属性验证:写法一
+ChildCpn.propTypes = {
+  // 字符串类型，必填
+  name: PropTypes.string.isRequired,
+  age: PropTypes.number,
+  height: PropTypes.string
+}
+
+// 默认值
+ChildCpn.defaultProps = {
+  name: 'loveZero',
+  age: 18,
+  height: '1.58'
+}
 // 函数组件
 function ChildFun (props) {
   const { name, age, height } = props
@@ -24,8 +48,9 @@ export default class Son extends Component {
   render () {
     return (
       <div>
-        <ChildCpn name='loveZero' age='18' height='1.58' />
-        <ChildCpn name='千玺' age='20' height='1.78' />
+        <ChildCpn name='loveZero' age={18} height='1.58' />
+        <ChildCpn name='千玺' age={20} height='1.78' />
+        <ChildCpn />
         <ChildFun name='loveZero' age='18' height='1.58' />
         <ChildFun name='千玺' age='20' height='1.78' />
       </div>
