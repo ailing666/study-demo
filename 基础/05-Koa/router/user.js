1acc2cb581719a1efc63695d8db925a2dadb00cd
TENCENT\v_llingai <1120877019@qq.com>
2021-12-14 10:46:27 +0800

    params,query

--- 基础/05-Koa/router/user.js
+++ 基础/05-Koa/router/user.js
@@ -3,12 +3,17 @@ const Router = require('koa-router');
 const router = new Router({prefix: "/users"});
 
 router.get('/', (ctx, next) => {
-  ctx.response.body = "get request~";
+  console.log(ctx.request.query);
+  ctx.response.body = ctx.request.query;
 });
 
 router.post('/', (ctx, next) => {
   ctx.response.body = "post request~";
 });
 
+router.get('/:id', (ctx, next) => {
+  console.log(ctx.request.params);
+  ctx.response.body = ctx.request.params;
+})
 
 module.exports = router;
