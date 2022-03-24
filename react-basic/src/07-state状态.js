daf40e1262b87c0e801d01520b2c906a72f8296c
TENCENT\v_llingai <1120877019@qq.com>
2022-03-24 16:04:43 +0800

    数据状态进阶用法

--- react-basic/src/07-state状态.js
+++ react-basic/src/07-state状态.js
@@ -2,12 +2,19 @@ import React from 'react'
 class HelloC extends React.Component {
   // 定义组件状态
   state = {
-    name: '小艾同学'
+    count: 1,
+    name: '小艾同学',
+    list: [1, 2, 3, 4],
+    person: {
+      name: '张三',
+      age: 18,
+      hobby: '打篮球'
+    }
   }
   btnClick = () => {
     // 必须使用setState方法修改状态
     this.setState({
-      name: 'loveZero'
+      count: this.state.count++
     })
   }
   render () {
