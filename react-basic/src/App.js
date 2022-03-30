700ae0b90dd2fb076816c4293931f364e6fbf032
loveZero <1120877019@qq.com>
2022-03-30 18:24:48 +0800

    小练习

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,5 +1,6 @@
-import { Input, Table } from 'antd'
+import { Input, Table, Space, Popconfirm } from 'antd'
 import React from 'react'
+import axios from 'axios'
 import './App.css'
 
 const { Search } = Input
@@ -27,18 +28,39 @@ class App extends React.Component {
         title: '操作',
         dataIndex: 'do',
         key: 'do',
+        render: (text, record) => (
+          <Space size="middle">
+            <Popconfirm title="确定要删除吗?"
+              onConfirm={() => this.handleDelete(record.id)}>
+              <a href="#">删除</a>
+            </Popconfirm>
+          </Space>
+        ),
       },
     ]
   }
-
+  componentDidMount () {
+    this.loadList()
+  }
   // 搜索
-  onSearch = (value) => {
-    console.log(value)
+  onSearch = async (value) => {
+    let res = await axios.get(`http://localhost:3001/data/?q=${value}`)
+    this.setState({
+      list: res.data
+    })
   }
   // 删除
-
+  handleDelete = async (id) => {
+    await axios.delete(`http://localhost:3001/data/${id}`)
+    this.loadList()
+  }
   // 加载列表
-
+  loadList = async () => {
+    const res = await axios.get('http://localhost:3001/data')
+    this.setState({
+      list: res.data
+    })
+  }
 
   render () {
     return (
@@ -52,6 +74,8 @@ class App extends React.Component {
             onSearch={this.onSearch}
           />
         </div>
+
+        <Table dataSource={this.state.list} columns={this.state.columns} />;
       </div>
     )
   }
