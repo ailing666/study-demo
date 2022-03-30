2ba0f9fa6f9b35e16ab031f46449d5fb19b90e1e
loveZero <1120877019@qq.com>
2022-03-30 15:18:47 +0800

    prop校验

--- react-basic/src/19-prop校验.js
+++ react-basic/src/19-prop校验.js
@@ -1,21 +1,37 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-function SonF ({ list }) {
+function SonF () {
   return (
-    <div>{list.map(item => <li key={item}>{item}</li>)}</div>
+    <div></div>
   )
 
 }
 SonF.propTypes = {
-  list: PropTypes.array
+  // 必填
+  list: PropTypes.array.isRequired,
+  // 必须是函数
+  handle: PropTypes.func,
+  // 必须是特定结构
+  obj: PropTypes.shape({
+    color: PropTypes.string,
+    fontSize: PropTypes.number,
+    optionalArray: PropTypes.array,
+    optionalBool: PropTypes.bool,
+    optionalFunc: PropTypes.func,
+    optionalNumber: PropTypes.number,
+    optionalObject: PropTypes.object,
+    optionalString: PropTypes.string,
+    optionalElement: PropTypes.element,
+  })
 }
 
+const styleObj = { color: 1, fontSize: 2 }
 class App extends React.Component {
+  handle = () => console.log(1)
   render () {
     return (
-      <SonF list={1}></SonF>
-      // <SonF list={[1, 2, 3, 4]}></SonF>
+      <SonF list={[1, 2, 3, 4]} handle={this.handle} obj={styleObj}></SonF >
     )
   }
 }
