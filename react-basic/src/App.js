8ea9808d1950dbde00a0ff8b01b08d67b1f1ac67
v_llingai <v_llingai@tencent.com>
2022-04-08 16:01:43 +0800

    编程式导航使用

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 // 引入路由跳转的组件
 import Home from './pages/Home.js'
 import About from './pages/About.js'
+import Login from './pages/Login.js'
 
 function App () {
   return (
@@ -11,11 +12,13 @@ function App () {
       {/* 用于指定导航链接，完成路由跳转，会渲染成a链接 */}
       <Link to="/">首页</Link>
       <Link to="/about">关于</Link>
+      <Link to="/login">登录</Link>
       {/* 提供路由出口，满足条件的路由组件会渲染到组件内部，定义path和组件的对应关系 */}
       <Routes>
         {/* 用于指定导航链接，完成路由匹配 */}
         <Route path='/' element={<Home />}></Route>
         <Route path='/about' element={<About />}></Route>
+        <Route path='/login' element={<Login />}></Route>
       </Routes>
     </BrowserRouter>
   )
---
+++ react-basic/src/pages/Login.js
@@ -0,0 +1,20 @@
+// 引入 useNavigate 钩子
+import { useNavigate } from 'react-router-dom'
+function Login () {
+  // 调用useNavigate赋值给navigate
+  const navigate = useNavigate()
+  const toAbout = () => {
+    // navigate传入要跳转的路由
+    navigate('/about')
+    // replace: true：表示在导航时不会向 history 添加新记录
+    // navigate('/about', { replace: true })
+  }
+  return (
+    <div>
+      Login
+      <button onClick={toAbout}>跳转到about</button>
+    </div>
+  )
+}
+
+export default Login
