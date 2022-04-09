111a1f09d7f7d39fad1bd0e82ba69f84f633f797
loveZero <1120877019@qq.com>
2022-04-09 16:29:03 +0800

    我的页面完成

--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -1,5 +1,41 @@
 <template>
-  <div class="user">user</div>
+  <div class="user">
+    <header>
+      <img src="../../assets/images/user.jpg" alt />
+      <div>
+        <h4 class="name">123456789@qq.com</h4>
+        <span>
+          文明驾驶分
+          <strong>0</strong>
+        </span>
+      </div>
+    </header>
+    <div class="user-links">
+      <ul>
+        <router-link to="aa" tag="a">
+          <i class="icon icon-user-link-1"></i>
+          我的帐户
+        </router-link>
+        <router-link to="aa" tag="a">
+          <i class="icon icon-user-link-2"></i>
+          租车订单
+        </router-link>
+        <router-link to="aa" tag="a">
+          <i class="icon icon-user-link-3"></i>
+          实名认证
+        </router-link>
+        <router-link to="aa" tag="a">
+          <i class="icon icon-user-link-4"></i>
+          安全设置
+        </router-link>
+        <router-link to="aa" tag="a">
+          <i class="icon icon-user-link-5"></i>
+          帮助中心
+        </router-link>
+      </ul>
+    </div>
+    <button class="login-out">登出</button>
+  </div>
 </template>
 
 <script>
@@ -15,13 +51,5 @@ export default {
 
 </script>
 <style lang='scss' scoped>
-.user {
-  position: fixed;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  width: 410px;
-  z-index: 101;
-  background-color: #32383c;
-}
+@import "./index.scss";
 </style>
