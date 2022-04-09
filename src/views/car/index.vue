eae2a8cc4b24dd95f3c8610e6cc19266297d1996
loveZero <1120877019@qq.com>
2022-04-09 10:55:14 +0800

    引入swiper样式调节

--- src/views/car/index.vue
+++ src/views/car/index.vue
@@ -1,19 +1,47 @@
 <template>
   <div>
     <div class="cars-wrap">
-      <CarList />
+      <swiper class="cars-swiper-container" :options="swiperOption">
+        <swiper-slide>
+          <CarList />
+        </swiper-slide>
+        <swiper-slide>
+          <CarList />
+        </swiper-slide>
+        <swiper-slide>
+          <CarList />
+        </swiper-slide>
+        <swiper-slide>
+          <CarList />
+        </swiper-slide>
+        <swiper-slide>
+          <CarList />
+        </swiper-slide>
+        <div class="swiper-button-prev" slot="button-prev"></div>
+        <div class="swiper-button-next" slot="button-next"></div>
+      </swiper>
     </div>
     <!-- <button @click="toUser">路由跳转</button> -->
   </div>
 </template>
 
 <script>
+import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
+import 'swiper/css/swiper.css'
 import CarList from '@/components/carList'
 export default {
   name: 'Car',
-  components: { CarList },
+  components: { CarList, Swiper, SwiperSlide },
   data () {
     return {
+      swiperOption: {
+        slidesPerView: 3,
+        spaceBetween: 50,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev'
+        }
+      }
     }
   },
   methods: {
@@ -23,14 +51,7 @@ export default {
     }
   }
 }
-
 </script>
-<style lang='scss' scoped>
-.cars-wrap {
-  position: fixed;
-  left: 0;
-  bottom: 94px;
-  right: 0;
-  z-index: 11;
-}
+<style lang="scss" scoped>
+@import "./index.scss";
 </style>
