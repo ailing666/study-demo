6b52d537138f4a97e460515c53d7dc8f42eeb52d
v_llingai <v_llingai@tencent.com>
2022-04-11 10:57:07 +0800

    嵌套路由

---
+++ react-basic/src/03-路由/嵌套路由/App.js
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Layout from './Layout'
+import Article from './Article'
+import Home from './Home'
+import Board from './Board'
+function App () {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/layout" element={<Layout />}>
+          {/* 嵌套二级路由 */}
+          <Route path="board" element={<Board />}></Route>
+          <Route path="article" element={<Article />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
