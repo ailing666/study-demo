d0fbb5d65ee2ea62839598292c92bf60d1da77d2
v_llingai <v_llingai@tencent.com>
2022-04-12 18:01:33 +0800

    配置路由

--- react-pc/src/App.js
+++ react-pc/src/App.js
@@ -1,10 +1,14 @@
-
-function App() {
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Layout from './pages/Layout'
+import Login from './pages/Login'
+function App () {
   return (
-    <div className="App">
-      App
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login />}></Route>
+        <Route path='/layout' element={<Layout />}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
-
-export default App;
+export default App
