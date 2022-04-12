461d3b221d4c725470a2b1486f0d423f34977fb5
v_llingai <v_llingai@tencent.com>
2022-04-12 19:07:18 +0800

    配置@路径

--- react-pc/src/App.js
+++ react-pc/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Layout from './pages/Layout'
-import Login from './pages/Login'
+// import Login from './pages/Login'
+import Login from '@/pages/Login'
+
 function App () {
   return (
     <BrowserRouter>
