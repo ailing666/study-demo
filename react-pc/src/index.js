4f9001b87dad00b6bec5e09ba4fffd845c6f300d
v_llingai <v_llingai@tencent.com>
2022-04-12 18:06:40 +0800

    引入antd

--- react-pc/src/index.js
+++ react-pc/src/index.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+// 导入 antd 样式
+import 'antd/dist/antd.min.css'
 import './index.scss'
 
 ReactDOM.render(
@@ -8,4 +10,4 @@ ReactDOM.render(
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
