3a4cc8b27c82f4c2845d5aa497c0908283429715
v_llingai <v_llingai@tencent.com>
2022-04-13 17:55:53 +0800

    home使用echarts

--- react-pc/src/pages/Home/index.js
+++ react-pc/src/pages/Home/index.js
@@ -1,6 +1,16 @@
+
+import Bar from "@/components/Bar"
+import './index.scss'
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="home">
+      <Bar
+        style={{ width: '500px', height: '400px' }}
+        xData={['vue', 'angular', 'react']}
+        sData={[50, 60, 70]}
+        title='三大框架满意度' />
+    </div>
   )
 }
+
 export default Home
