ac7c204370f063a5362de17c1c4191113f712256
v_llingai <v_llingai@tencent.com>
2022-04-19 11:14:58 +0800

    打包css

--- 02-局部webpack/src/js/api.js
+++
@@ -1,4 +0,0 @@
-const sum = (x, y) => x + y
-
-const square = x => x * x
-export { sum, square }
--- 02-局部webpack/webpack.config.js
+++ 02-局部webpack/webpack.config.js
@@ -1,6 +1,7 @@
 
 const path = require('path')
 module.exports = {
+  mode: 'production',
   // 配置入口文件
   entry: "./src/index.js",
   // 指定输出位置
@@ -9,5 +10,16 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     // 打包生成的js名称
     filename: 'build.js',
+  },
+  module: {
+    // 指定多个配置规则
+    rules: [
+      {
+        // 正则表达式：匹配以 .css 结尾的文件
+        test: /\.css$/i,
+        // use 中 loader 的加载顺序：先下后上，先右后左，也就是所先执行 css-loader 再执行 style-loader
+        use: ['style-loader', 'css-loader']
+      }
+    ]
   }
 } 
